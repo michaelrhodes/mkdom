bb25045e259f806101497d9bfbbc3046d45260b7
michaelrhodes <spam@michaelrhod.es>
2014-09-24 21:40:37 +1000

    Made complete document test server-only.

--- test/shared/tests.js
+++ test/shared/tests.js
@@ -20,6 +20,12 @@ var text = function(element, content) {
 module.exports = function(mkdom) {
 
   run('complete document', function (test) {
+    if (process.browser) {
+      test.pass('this is a server-only test')
+      test.end()
+      return
+    }
+
     var element = mkdom('<!doctype html>\n<html>\n  <head>\n    <title>Complete document</title>\n  </head>\n  <body>\n    <p class="introduction">This is the introduction.</p>\n  </body>\n</html>\n')
     
     var title = element.getElementsByTagName('title')[0]
