13568c509a1cc35e52d3384f22c71a4925837374
michaelrhodes <spam@michaelrhod.es>
2019-03-14 12:00:21 +1000

    Enable use as a tag function

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 var domino = require('domino')
 var wrap = require('./wrap')
 
-var complete = /^\s*<(!doctype|html)/i
-var browser = typeof document == 'object'
-var doc = browser ? document : domino.createDocument()
+var full = /^\s*<(!doctype|html)/i
+var node = typeof document !== 'object'
+var doc = node ? domino.createDocument() : document
 
 module.exports = mkdom
 
 function mkdom (html) {
+  // Enable use as a tag function
+  if (html.raw) return mkdom(String.raw.apply(String, arguments))
+
   // Remove any surrounding plain text
   html = html.replace(/(^[^<]*|[^>]*$)/, '')
 
-  return !browser && complete.test(html) ?
+  // Full pages need their own document object
+  return node && full.test(html) ?
     domino.createDocument(html) :
     partial(html, doc)
 }
@@ -28,7 +32,7 @@ function partial (html, doc) {
   var child = el.childNodes
   var i = 0, l = child.length
   for (var c = 0; i < l; i++)
-    if (child[i].nodeType == 3 && ++c > 1)
+    if (child[i].nodeType === 3 && ++c > 1)
       return el
 
   // Return enclosed elements without wrapper
